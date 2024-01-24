868d2e3d82c3263b0f5533a2c7054ad15a1382fb
Aryman Sharma <aryman.sharma125@gmail.com>
2024-01-25 03:42:52 +0530

    JS Completed

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateButton");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 
+const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
 let password = "";
 let passwordLength = 10;
-let CheckCount = 1;
+let checkCount = 1;
 
 handleSlider();
 
@@ -31,5 +33,144 @@ function setIndicator(color)
 
 function getrandomInteger(min,max)
 {
-   Math.floor(Math.random()*(max-min)) + min;
+  return  Math.floor(Math.random()*(max-min)) + min;
+
 } 
+
+function generateRandomNumber()
+{
+ return getrandomInteger(0,9);
+}
+
+function generateLowercase()
+{
+ return String.fromChar(getrandomInteger(97,123));
+}
+
+function generateUppercase()
+{
+ return String.fromChar(getrandomInteger(65,91));
+}
+
+function generateSymbol()
+{
+    const randomNUM = getrandomInteger(0,symbols.length);
+    return symbols.charAt(randomNUM);
+}
+function calcStrength()
+{
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
+
+async function copyContent()
+{  
+    try{
+        await navigator.clipboard.writeText(passwordDisplay.value);
+    // this returns a promise which can be either resloved or give an error 
+     copyMsg.innerText="copied";
+    }
+    catch(error)
+    {
+      copyMsg.innerText="failed";
+    }
+    // To make copy wala span visible
+    copyMsg.classList.add("active");
+    
+    setTimeout(() =>
+    {
+        copyMsg.classList.remove("active");
+    },2000);
+
+}
+inputSlider.addEventListener('input',(e)=>
+{
+    passwordLength=e.target.value;
+    handleSlider();
+});
+
+copyBtn.addEventListener('click',()=>
+{
+    if(passwordDisplay.value)
+    copyContent();
+});
+
+function handleCheckBoxChange() {
+    checkCount = 0;
+    allCheckBox.forEach( (checkbox) => {
+        if(checkbox.checked)
+            checkCount++;
+    });
+
+    //special condition
+    if(passwordLength < checkCount ) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+}
+
+allCheckBox.forEach( (checkbox) => {
+    checkbox.addEventListener('change', handleCheckBoxChange);
+})
+
+
+
+generateBtn.addEventListener('click',()=>{
+
+    // none of the checkbox are selected
+    
+    if(checkCount <=0) return;
+
+    if(passwordLength < checkCount)
+    {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+
+ password="" ;
+ 
+//  if(uppercaseCheck.checked)
+//  {
+//     password+=generateUpperCase();
+//  }
+//  if(lowercaseCheck.checked)
+//  {
+//     password+=generatellowerCase();
+//  }
+//  if(numbersCheck.checked)
+//  {
+//     password+=generateRandomNumber();
+//  }
+//  if(symbolsCheck.checked)
+//  {
+//     password+=generateSymbol();
+//  }
+
+let funcArr=[];
+
+
+
+
+
+
+});
+
+
